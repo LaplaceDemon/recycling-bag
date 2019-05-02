f3b53e455a3c6023d66ac654081dedcd19a8d52e
jackie.sjq <jackie.sjq@Windows10>
2019-05-02 14:35:10 +0800

    Modify a bug in the object pool

--- src/main/java/io/github/laplacedemon/recyclingbag/RecyclingBag.java
+++ src/main/java/io/github/laplacedemon/recyclingbag/RecyclingBag.java
@@ -4,52 +4,58 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.function.Supplier;
 
 public class RecyclingBag<T> {
-    private ArrayBlockingQueue<T> pool = new ArrayBlockingQueue<T>(1024);
+    private ArrayBlockingQueue<T> publicPool = new ArrayBlockingQueue<T>(1024);
     private ThreadLocal<ArrayQueue<T>> localVar;
     private Supplier<? extends T> supplier;
+    private final int localNum;
     
-    public RecyclingBag(Supplier<? extends T> supplier) {
-        this.supplier = supplier;
-        this.localVar = ThreadLocal.withInitial(()->{
-            final ArrayQueue<T> queue = new ArrayQueue<T>(1024);
+    public RecyclingBag(final int localNum, final long publicNum, final Supplier<? extends T> supplier) {
+        System.out.println("创建内存池");
+    	this.supplier = supplier;
+        this.localNum = localNum;
+        this.localVar = ThreadLocal.withInitial(() -> {
+        	String threadName = Thread.currentThread().getName();
+        	System.out.println("创建线程:" + threadName);
+            final ArrayQueue<T> queue = new ArrayQueue<T>(localNum);
             
-            for(int i = 0;i<1024;i++) {
+            for(int i = 0;i<localNum;i++) {
                 T x = supplier.get();
                 queue.add(x);
             }
-            System.out.println("返回queue");
+            
             return queue;
         });
         
-        for(int i = 0; i < 1024; i++) {
+        for(int i = 0; i < publicNum; i++) {
             T x = supplier.get();
-            this.pool.add(x);
+            this.publicPool.add(x);
         }
     }
     
     public T get() {
-        T x = this.localVar.get().take();
+    	ArrayQueue<T> queue = this.localVar.get();
+        T x = queue.take();
         if(x != null) {
             return x;
         }
         
-        x = this.pool.poll();
+        x = this.publicPool.poll();
         if(x != null) {
-            System.out.println("从公共池中获取");
             return x;
         }
-        System.out.println("JVM获取");
+        
         return supplier.get();
     }
     
     public void release(T obj) {
         ArrayQueue<T> queue = this.localVar.get();
-        if(queue.size() >= 1024) {
-            System.out.println("归还到公共池");
-            this.pool.add(obj);
+        if(queue.size() >= this.localNum) {
+            this.publicPool.add(obj);
             return ;
         }
-        queue.add(obj);
+        queue.release(obj);
     }
+    
+    
     
 }
--- src/test/java/io/github/laplacedemon/recyclingbag/PointerPool.java
+++ src/test/java/io/github/laplacedemon/recyclingbag/PointerPool.java
@@ -1,8 +1,6 @@
 package io.github.laplacedemon.recyclingbag;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 import org.junit.Test;
@@ -10,16 +8,34 @@ import org.junit.Test;
 public class PointerPool {
     
     @Test
-    public void testGetPointer() {
-        RecyclingBag<Pointer> recyclingBag = new RecyclingBag<>(()->{
+    public void testGetPointer() throws InterruptedException {
+        RecyclingBag<Pointer> recyclingBag = new RecyclingBag<>(1024, 1024,()->{
             return new Pointer();
         });
         
         for(int i = 0;i<1024*2 + 1;i++) {
             Pointer pointer = recyclingBag.get();
             System.out.println(i + "," + pointer);
+            recyclingBag.release(pointer);
+        }
+    }
+    
+    @Test
+    public void testGetPointerMultithreading() throws InterruptedException {
+        RecyclingBag<Pointer> recyclingBag = new RecyclingBag<>(1024, 1024,()->{
+            return new Pointer();
+        });
+        
+        for(int n = 0; n < 10; n++) {
+        	new Thread(() -> {
+            	for(int i = 0; i < 1024*2 + 1; i++) {
+                    Pointer pointer = recyclingBag.get();
+                    recyclingBag.release(pointer);
+                }
+            }, "thread-" + n).start();
         }
         
+        Thread.sleep(1000*20);
     }
     
     @Test
@@ -59,7 +75,8 @@ public class PointerPool {
             queue.add(new Pointer());
         }
         
-        long size = 10000*10000*20L;
+        long size = 10000*10000*10L;
+        
         long t0 = System.currentTimeMillis();
         for(long i = 0;i < size;i++) {
             Pointer ele = queue.take();
@@ -72,11 +89,11 @@ public class PointerPool {
     
     @Test
     public void testGetAndBack() {
-        RecyclingBag<Pointer> recyclingBag = new RecyclingBag<>(()->{
+        RecyclingBag<Pointer> recyclingBag = new RecyclingBag<>(1024, 1024, ()->{
             return new Pointer();
         });
         
-        long size = 10000*10000L;
+        long size = 10000*10000*10L;
         
         long t0 = System.currentTimeMillis();
         for(long i = 0;i<size;i++) {
